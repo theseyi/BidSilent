eb2fcd26c768d239ffab6b94525032f5bd856285
Mark Ethan Trostler <mark@zzo.com>
2012-03-09 04:15:56 +0300

    admin stuff

--- server/public/loadModules.js
+++ server/public/loadModules.js
@@ -1,49 +1,21 @@
 YUI().add('loadModules', function(Y) {
-    var done = function(Y) { Y.Global.Hub.fire('modulesLoaded'); }
-        , err = function() { Y.log('Error loading modules'); Y.log(arguments); }
-        , modules = []
-        , mod
-        , mods = {
-            nav: {
-                requires: [ 'node-menunav' ]
-                , fullpath: '/modules/nav.js'
-            }
-            , login: {
-                requires: [ 'node', 'panel', 'dd-plugin' ]
-                , fullpath: '/modules/login.js'
-            }
-            , register: {
-                requires: [ 'node', 'panel', 'dd-plugin' ]
-                , fullpath: '/modules/reg.js'
-            }
-            , error: {
-                requires: [ 'node', 'panel', 'dd-plugin' ]
-                , fullpath: '/modules/error.js'
-            }
-            , user: {
-                requires: [ 'node', 'panel', 'dd-plugin' ]
-                , fullpath: '/modules/user.js'
-            }
-            , tokens: {
-                requires: [ 'node', 'panel', 'dd-plugin' ]
-                , fullpath: '/modules/tokens.js'
-            }
-/*
-            , admin: {
-                requires: [ 'node', 'cookie' ]
-                , fullpath: '/modules/admin.js'
-            }
-*/
+    var done = function(Y) { Y.Global.Hub.fire('modulesLoaded'); };
+
+    Y.LoadModules = function(mods, errFun) {
+        var mod
+            , modules = []
+            , err = errFun || function() { Y.log('Error loading modules'); Y.log(arguments); }
+        ;
+
+        // Use them all
+        for (mod in mods) {
+            modules.push(mod);
         }
 
-    // Use them all
-    for (mod in mods) {
-        modules.push(mod);
+        // Add 'done' function
+        modules.push(done);
+
+        // Let 'er rip
+        Y.use.apply(YUI({ modules: mods, loadErrorFn: err }), modules);
     }
-
-    // Add 'done' function
-    modules.push(done);
-
-    // Let 'er rip
-    Y.use.apply(YUI({ modules: mods, loadErrorFn: err }), modules);
 });
--- server/server.js
+++ server/server.js
@@ -1,36 +1,46 @@
 var connect = require('connect')
+    , docRoot = 'server/public'
+    , fs = require('fs')
     , server = connect.createServer(
         connect.logger()
         , connect.cookieParser()
+        , connect.favicon()
         , function(req, res, next) {
-            if (req.url !== '/admin') {
-                next();
-            } else {
+            var localFile = req.url;
+            if (req.url.match("^/admin")) {
                 eventHub.emit('user:getProfile', { 'eventHub:session': req.cookies.eventhub }, function(err, profile) {
                     if (!err) {
                         if (profile.admin) {
-                            console.log('ADMIN USER');
-                            res.end('HELLO ADMIN USER');
+                            localFile = '/admin.html';
                         } else {
-                            // 404
-                            res.statusCode = 404;
-                            res.end('u no admin user');
+                            localFile = '/';
                         }
                     } else {
-                        // 404
-                        res.statusCode = 404;
-                        res.end('u no admin user: ' + err);
+                        localFile = '/';
                     }
+                    sendFile(res, localFile);
                 });
+            } else {
+                sendFile(res, localFile);
             }
         }
-        , connect.static(__dirname + '/public')
     )
     , io = require('socket.io').listen(server)
     , port = 80
     , fs = require('fs')
     , path = require('path')
     , eventHub = require('EventHub/clients/server/eventClient.js').getClientHub('http://localhost:5883?token=ehrox')
+    , sendFile = function(res, file) {
+        if (file === '/') {
+            file = '/index.html';
+        }
+        fs.readFile(docRoot + file, 'utf8', function(err, data) {
+            if (err) {
+                res.responseCode = 404;
+            }
+            res.end(data);
+        });
+    }
     ;
 
 // get ref to hub & then load server-side modules
@@ -39,12 +49,10 @@ eventHub.on('eventHubReady', function() {
     modules.forEach(function(fname) {
         if (fname.match('.js$')) {
             require(path.join(__dirname, 'modules', fname)).create(eventHub);
-            console.log('loaded: ' + fname);
         }
     });
 });
 
 server.listen(port);
 console.log('Listening on port ' + port);
-
 
